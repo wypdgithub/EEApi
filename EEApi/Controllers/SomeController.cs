96fea19464fe9f076a4ee0d104885794c425b3a8
wypdgithub <71215481+wypdgithub@users.noreply.github.com>
2020-10-14 08:40:04 +0800

    教师增删改查

--- EEApi/Controllers/SomeController.cs
+++ EEApi/Controllers/SomeController.cs
@@ -24,11 +24,45 @@ namespace EEApi.Controllers
         //{
         //    return mc.emps.ToList();
         //}
+        //显示
         [HttpGet]
-        public List<Emp> Show()
+        public List<Emp> Show(string Ename)
         {
             string sql = "select * from Emp where EState=0";
+            var list= db.GetToList<Emp>(sql);
+
+            if (!string.IsNullOrEmpty(Ename))
+            {
+                list = list.Where(s => s.Name.Contains(Ename)).ToList();
+            }
+            return list;
+
+        }
+        //修改状态/假删除
+        [HttpGet]
+        public int UpdState(int id)
+        {
+            string sql = $"update Emp set EState=1 where Id={id}";
+            return db.ExecuteNonQuery(sql);
+        }
+        [HttpPost]
+        public int Add(Emp m)
+        {
+            string sql = $"insert into Emp values('{m.Name}',{m.Age},0)";
+            return db.ExecuteNonQuery(sql);
+        }
+        [HttpGet]
+        public List<Emp> Find(int id)
+        {
+            string sql = $"select * from Emp where Id={id}";
             return db.GetToList<Emp>(sql);
+        }
+        //修改
+        [HttpPost]
+        public int UpdEmp(Emp m)
+        {
+            string sql = $"update Emp set Name='{m.Name}' ,Age='{m.Age}' where Id={m.Id}";
+            return db.ExecuteNonQuery(sql);
         }
     }
 }
